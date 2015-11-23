adebcd7666b75ccf7b05af8941ccc72384b6a9bc
andreasalama <andreasalama2@gmail.com>
2015-11-23 18:46:39 -0500

    strncat

---
+++ ft_strncat.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+char	*ft_strncat(char *s1, const char *s2, size_t n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+		i++;
+	while (j :wq< n)
+	{
+		s1[i] = s2[j];
+		i++;
+		j++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
